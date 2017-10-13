67c306f949be49fe56f067530ef3dc5dc4ff49f4
Aleksandr Chermenin <aleksandr_chermenin@epam.com>
2017-10-13 14:54:49 +0300

    Some fixes and improvements.

--- src/main/java/org/apache/flink/contrib/siddhi/operator/StreamRecordComparator.java
+++ src/main/java/org/apache/flink/contrib/siddhi/operator/StreamRecordComparator.java
@@ -31,12 +31,6 @@ public class StreamRecordComparator<IN> implements Comparator<StreamRecord<IN>>,
 
     @Override
     public int compare(StreamRecord<IN> o1, StreamRecord<IN> o2) {
-        if (o1.getTimestamp() < o2.getTimestamp()) {
-            return -1;
-        } else if (o1.getTimestamp() > o2.getTimestamp()) {
-            return 1;
-        } else {
-            return 0;
-        }
+        return Long.compare(o1.getTimestamp(), o2.getTimestamp());
     }
 }
